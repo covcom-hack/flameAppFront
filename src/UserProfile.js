d1293256c82d23caa055144d3f4d52323b24d269
Sitkevich88 <Sitkevich2002@mail.ru>
2022-11-20 09:58:02 +0300

    updated components

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -14,64 +14,72 @@ const UserProfile = (props) => {
 
     return <>
         <UserHeader/>
-        <p className={styles.title}>Личные данные</p>
-        <div className={styles.card}>
-            <div className={styles.fields}>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Фамилия:</span>
-                    <p className={styles.text}>{user.lastName}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Имя:</span>
-                    <p className={styles.text}>{user.firstName}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Отчество:</span>
-                    <p className={styles.text}>{user.patronymicName}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Номер паспорта:</span>
-                    <p className={styles.text}>{user.passport}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>ИНН:</span>
-                    <p className={styles.text}>{user.inn}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Номер телефона:</span>
-                    <p className={styles.text}>{user.phoneNumber}</p>
-                </div>
-                <div className={styles.labelAndText}>
-                    <span className={styles.label}>Логин:</span>
-                    <p className={styles.text}>{user.login}</p>
+        <div className={styles.cardsGrid}>
+            <div className={styles.titleAndCardWrapper}>
+                <p className={styles.title}>Личные данные</p>
+                <div className={styles.card}>
+                    <div className={styles.fields}>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Фамилия:</span>
+                            <p className={styles.text}>{user.lastName}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Имя:</span>
+                            <p className={styles.text}>{user.firstName}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Отчество:</span>
+                            <p className={styles.text}>{user.patronymicName}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Номер паспорта:</span>
+                            <p className={styles.text}>{user.passport}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>ИНН:</span>
+                            <p className={styles.text}>{user.inn}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Номер телефона:</span>
+                            <p className={styles.text}>{user.phoneNumber}</p>
+                        </div>
+                        <div className={styles.labelAndText}>
+                            <span className={styles.label}>Логин:</span>
+                            <p className={styles.text}>{user.login}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <p className={styles.title}>Счета</p>
-        <table className={styles.openedBankAccounts}>
-            <thead>
-            <tr>
-                <th>Валюта</th>
-                <th>Бланс</th>
-                <th>Номер счета</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>Рубль</td>
-                <td>1000</td>
-                <td>1234567890</td>
-            </tr>
-            <tr>
-                <td>Евро</td>
-                <td>47.65</td>
-                <td>1234567891</td>
-            </tr>
-            </tbody>
-        </table>
-        <p className={styles.title}>Открытие счетов</p>
-        <div className={styles.bankAccounts}>
+            <div className={styles.titleAndCardWrapper}>
+                <p className={styles.title}>Счета</p>
+                <table className={styles.openedBankAccounts}>
+                    <thead>
+                    <tr>
+                        <th>Валюта</th>
+                        <th>Бланс</th>
+                        <th>Номер счета</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Рубль</td>
+                        <td>1000</td>
+                        <td>1234567890</td>
+                    </tr>
+                    <tr>
+                        <td>Евро</td>
+                        <td>47.65</td>
+                        <td>1234567891</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className={styles.titleAndCardWrapper}>
+                <p className={styles.title}>Открыть счёт</p>
+                <div className={styles.bankAccounts}>
 
+                </div>
+            </div>
         </div>
     </>;
 }
